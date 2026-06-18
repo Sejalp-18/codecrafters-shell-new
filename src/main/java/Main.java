60f3d638fd042af742227cfb9a13872bcaeb9aba
Sejalp-18 <pednekarsejal18@gmail.com>
2026-06-18 16:39:19 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,12 @@ public class Main {
         while (true) {
             System.out.print("$ ");
             String s = sc.nextLine();
-            if (s.equals("exit"))
-                return;
-            else if (s.startsWith("echo"))
-                System.out.println(s.substring(5));
+            if (s.endsWith("exit"))
+                System.out.println("exit is a shell builtin");
+            else if (s.endsWith("echo"))
+                System.out.println("echo is a shell builtin");
             else
-                System.out.println(s + ": command not found");
+                System.out.println(s + ": not found");
         }
 
     }
